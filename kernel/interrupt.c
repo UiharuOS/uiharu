4169d7c74bd4ee879d2542f9933bb088c0078be3
neo1218 <neo1218@yeah.net>
2016-12-05 18:50:36 +0800

    update kernel interrupt, add 8259A setting, lidt

--- kernel/interrupt.c
+++ kernel/interrupt.c
@@ -7,8 +7,13 @@
 #include "interrupt.h"
 #include "stdint.h"  // 定义各种数据类型
 #include "global.h"
+#include "io.h"
 
 #define IDT_DESC_CNT 0x21  // 目前总共支持0x21(33)个中断向量
+#define PIC_M_CTRL 0x20    // PIC主片控制端口号 
+#define PIC_M_DATA 0x21    // PIC主片数据端口号
+#define PIC_S_CTRL 0xa0    // PIC从片控制端口号
+#define PIC_S_DATA 0xa1    // PIC从片数据端口号
 
 struct gate_desc {
     // 中断门描述符结构体
@@ -22,11 +27,7 @@ struct gate_desc {
 // static 函数声明
 static void make_idt_desc(struct gate_desc* p_gdesc, uint8_t attr, intr_handler function);
 static struct gate_desc idt[IDT_DESC_CNT];
-/* idt: 创建中断门描述符, idt本质上就是中断门描述符数组
- *  - IDT_DESC_CNT: 中断向量的个数
- */
-// extern 外部变量
-// 引用kernel.S中定义的中断向量数组, 对应相应的中断处理例程
+// extern 外部对象声明
 extern intr_handler intr_entry_table[IDT_DESC_CNT];
 
 static void make_idt_desc(struct gate_desc* p_gdesc, uint8_t attr, intr_handler function) {
@@ -50,7 +51,39 @@ static void idt_desc_init(void) {
         // intr_entry_table[i] 第i个中断向量对应的中断处理例程
         make_idt_desc(&idt[i], IDT_DESC_ATTR_DPL0, intr_entry_table[i]);
     }
-    print_string("idt_desc_init done\n");
+    print_string("idt desc init done\n");
+}
+
+static void pic_init(void) {
+    /* pic_init: 初始化8259A PIC芯片
+     */
+    // 初始化主片
+    // 往主片中写入ICW1(设置工作模式: 边沿触发、级联)
+    outb (PIC_M_CTRL, 0x11); // ic4为1, 后需写入ic4寄存器
+                             // sngl为0表示采用级联模式
+                             // 设置8085CPU的调用时间间隔(兼容无设)
+                             // LTIM为0设置边沿触发方式
+    // 往主片中写入ICW2(设置起始中断向量号)
+    outb (PIC_M_DATA, 0x20); // 起始中断向量号32, 其余由IRQ接口顺延
+    // 往主片中写入ICW3(设置主从级联时用到的引脚)
+    outb (PIC_M_DATA, 0x04); // 使用S2引脚连接主片和从片
+    // 往主片中写入ICW4
+    outb (PIC_M_DATA, 0x01); // 设置uPM位为1(x86处理器)
+
+    // 初始化从片
+    // 往从片中写ICW1, 同主片设置
+    outb (PIC_S_CTRL, 0x11);
+    // 往从片中写ICW2
+    outb (PIC_S_DATA, 0x28); // 每个片对应着8个中断向量
+    outb (PIC_S_DATA, 0x02); // 从片使用S1引脚连接
+    outb (PIC_S_DATA, 0x01); // 设置uPM位为1(x86处理器)
+
+    // 打开主片上IR0, 接受时钟产生的中断
+    // 此时向8259A发送的命令操作属于OCW
+    outb (PIC_M_DATA, 0xfe);
+    oubb (PIC_S_DATA, 0xff);
+
+    print_string("pic init done!\n");
 }
 
 void idt_init() {
@@ -59,11 +92,16 @@ void idt_init() {
     print_string("idt_init start\n");
     idt_desc_init(); // 初始化中断描述符
     pic_init();      // 初始化中断代理芯片(ex:8259A)
-
+    // 加载idt
     uint64_t idt_operand = (
         (sizeof(idt) - 1) | ((uint64_t)(
             (uint32_t)idt << 16)
         )
-    );
-    print_string("idt_init done\n");
+    ); // 组装IDTR
+    asm volatile (
+        "lidt %0"
+        :
+        : "m" (idt_operand)
+    );  // 加载idt
+    print_string("idt init done\n");
 }
