5e8ca7080f93e491c92709e1006fd4e4286c1662
neo1218 <neo1218@yeah.net>
2016-12-10 17:54:37 +0800

    update idt

--- kernel/interrupt.c
+++ kernel/interrupt.c
@@ -84,14 +84,14 @@ static void exception_init(void) {
     // 初始化20个中断异常
     intr_name[0] = "#DE Divide Error";
     intr_name[1] = "#DB Debug Exception";
-    intr_name[2] = "NMI Interrupt";
+    intr_name[2] = "    NMI Interrupt";
     intr_name[3] = "#BP Breakpoint Exception";
     intr_name[4] = "#OF Overflow Exception";
     intr_name[5] = "#BR BOUND Range Exceeded Exception";
     intr_name[6] = "#UD Invalid Opcode Exception";
     intr_name[7] = "#NM Device Not Available Exception";
     intr_name[8] = "#DF Double Fault Exception";
-    intr_name[9] = "Coprocessor Segment Overrun";
+    intr_name[9] = "#MF Coprocessor Segment Overrun";
     intr_name[10]= "#TS Invalid TSS Exception";
     intr_name[11]= "#NP Segment Not Present";
     intr_name[12]= "#SS Stack Fault Exception";
@@ -101,7 +101,7 @@ static void exception_init(void) {
     intr_name[16]= "#MF x87 FPU Floating-Point Error";
     intr_name[17]= "#AC Alignment Check Exception";
     intr_name[18]= "#MC Machine-check Exception";
-    intr_name[19]= "#XF SIMD Floating-Point Exception";
+    intr_name[19]= "#XM SIMD Floating-Point Exception";
 }
 
 static void pic_init(void) {
