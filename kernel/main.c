89fa089fcd6a1e400980a95573dac375af3665a6
neo1218 <neo1218@yeah.net>
2016-12-14 18:10:14 +0800

    test ASSERT

--- kernel/main.c
+++ kernel/main.c
@@ -6,9 +6,12 @@
 
 #include "print.h"
 #include "init.h"
+#include "debug.h"
+
 void main(void) {
     print_string("I am kernel\n");
     init_all();
     asm volatile ("sti");  // 打开中断
+    ASSERT(1==2);
     while(1); // 操作系统就是一个中断(事件)驱动的死循环(loop)
 }
