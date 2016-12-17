ab39ea2e43417222a5dd637bb2bf15d50a194504
neo1218 <neo1218@yeah.net>
2016-12-17 12:45:55 +0800

    update kernel main thread

--- kernel/main.c
+++ kernel/main.c
@@ -7,11 +7,18 @@
 #include "print.h"
 #include "init.h"
 #include "debug.h"
+#include "string.h"
+#include "stdint.h"
 
-void main(void) {
+int main(void) {
     print_string("This is uiharu kernel\n");
     init_all();
     // asm volatile ("sti");  // 打开中断
     // ASSERT(1==2);
+    void* addr = get_kernel_pages(3);
+    print_string("\nTest)-> get_kernel_page start vaddr is ");
+    print_int((uint32_t*)addr, 'H');
+    print_string("\n");
     while(1); // 操作系统就是一个中断(事件)驱动的死循环(loop)
+    return 0;        
 }
--- kernel/memory.h
+++ kernel/memory.h
@@ -9,6 +9,14 @@
 #include "stdint.h"
 #include "bitmap.h"
 
+// 定义属性位用于页表修改
+#define PG_P_1   1  // 页表项或页目录项存在的属性位, 存在
+#define PG_P_0   0  // 页表项或页目录项存在的属性位, 不存在
+#define PG_RW_R  0  // R/W 属性位值, 读/执行
+#define PG_RW_W  2  // R/W 属性位值, 读/写/执行
+#define PG_US_S  0  // U/S 属性位值, 系统级
+#define PG_US_U  4  // U/S 属性位值, 用户级
+
 /* 虚拟内存地址池 */
 struct virtual_addr {
     struct bitmap vaddr_bitmap;  // 虚拟地址池位图
@@ -24,21 +32,20 @@ struct pool {
     uint32_t pool_size;          // 该内存池的字节容量
 };
 
-// for extern
-extern struct pool kernel_pool, user_pool;
-void mem_init(void);
-
 enum pool_flags {
     PF_KERNEL = 1,  // 内核物理内存池
     PF_USER = 2     // 用户物理内存池
 };
 
-// 定义属性位用于页表修改
-#define PG_P_1   1  // 页表项或页目录项存在的属性位, 存在
-#define PG_P_0   0  // 页表项或页目录项存在的属性位, 不存在
-#define PG_RW_R  0  // R/W 属性位值, 读/执行
-#define PG_RW_W  2  // R/W 属性位值, 读/写/执行
-#define PG_US_S  0  // U/S 属性位值, 系统级
-#define PG_US_U  4  // U/S 属性位值, 用户级
+// for extern
+extern struct pool kernel_pool, user_pool;
+void mem_init(void);
+static void* vaddr_get(enum pool_flags, uint32_t);
+uint32_t* pte_ptr(uint32_t);
+uint32_t* pde_ptr(uint32_t);
+static void* palloc(struct pool*);
+static void page_table_add(void*, void*);
+void* malloc_page(enum pool_flags, uint32_t);
+void* get_kernel_pages(uint32_t);
 
 #endif
