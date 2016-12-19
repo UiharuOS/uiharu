8b9243d280501c39322685686731ffb823be45f9
neo1218 <neo1218@yeah.net>
2016-12-19 15:32:14 +0800

    update device disk

--- kernel/device/disk.c
+++ kernel/device/disk.c
@@ -1,9 +1,4 @@
-/***
- * inline assembly:
- * --> its just some assembly routines written as inline functions
- * --> "constraint" (C expression) <- Operands
- */
-#include "stdint.h"
+typedef unsigned int uint32_t;
 
 void read_from_disk(uint32_t sector_no, uint32_t start_sector, uint32_t mem_to) {
     /* read_from_disk: 从硬盘中读取数据到指定内存
@@ -11,46 +6,47 @@ void read_from_disk(uint32_t sector_no, uint32_t start_sector, uint32_t mem_to) 
      *   - start_sector: 起始lba扇区号
      *   - mem_to: 指定内存
      */
-    asm volatile ("movl %eax, %esi"
-                  "movw %cx, %di"
-                  "movw $0x1f2, %dx"
-                  "movw %cl, %al"
-                  "outw %al, %dx"
-                  "movl %esi, %eax"
-                  "movw $0x1f3, %dx"
-                  "outw %al, %dx"
-                  "movw $8, %cl"
-                  "shr %cl, %eax"
-                  "movw $0x1f4, %dx"
-                  "outw %al, %dx"
-                  "shr %cl, %eax"
-                  "movw $0x1f5, %dx"
-                  "outw %al, %dx"
-                  "shr %cl, %eax"
-                  "or $0xe0, %al"
-                  "movw $0x1f6, %dx"
-                  "outw %al, %dx"
-                  "movw $0x1f7, %dx"
-                  "movw $0x20, %al"
-                  "outw %al, %dx"
-                  ".not_ready:"
-                  "nop"
-                  "in %dx, %al"
-                  "and $0x88, %al"
-                  "cmp $0x88, %al"
-                  "jnz .not_ready"
-                  "movw %di, %ax"
-                  "movw $256, %dx"
-                  "mul %dx"
-                  "movw %ax, %cx"
-                  "movw $0x1f0, %dx"
-                  ".go_on_read:"
-                  "in %dx, %ax"
-                  "movw %ax, (%bx)"
-                  "add $2, %bx"
-                  "loop .go_on_read"
+    asm volatile ("movl %%eax, %%esi\n\t"
+                  "movw %%cx, %%di\n\t"
+                  "movw $0x1f2, %%dx\n\t"
+                  "movb %%cl, %%al\n\t"
+                  "outb %%al, %%dx\n\t"
+                  "movl %%esi, %%eax\n\t"
+                  "movw $0x1f3, %%dx\n\t"
+                  "outb %%al, %%dx\n\t"
+                  "movb $8, %%cl\n\t"
+                  "shr %%cl, %%eax\n\t"
+                  "movw $0x1f4, %%dx\n\t"
+                  "outb %%al, %%dx\n\t"
+                  "shr %%cl, %%eax\n\t"
+                  "movw $0x1f5, %%dx\n\t"
+                  "outb %%al, %%dx\n\t"
+                  "shr %%cl, %%eax\n\t"
+                  "or $0xe0, %%al\n\t"
+                  "movw $0x1f6, %%dx\n\t"
+                  "outb %%al, %%dx\n\t"
+                  "movw $0x1f7, %%dx\n\t"
+                  "movb $0x20, %%al\n\t"
+                  "outb %%al, %%dx\n\t"
+                  ".not_ready:\n\t"
+                  "nop\n\t"
+                  "in %%dx, %%al\n\t"
+                  "and $0x88, %%al\n\t"
+                  "cmp $0x88, %%al\n\t"
+                  "jnz .not_ready\n\t"
+                  "movw %%di, %%ax\n\t"
+                  "movw $256, %%dx\n\t"
+                  "mul %%dx\n\t"
+                  "movw %%ax, %%cx\n\t"
+                  "movw $0x1f0, %%dx\n\t"
+                  ".go_on_read:\n\t"
+                  "in %%dx, %%ax\n\t"
+                  "movw %%ax, (%%bx)\n\t"
+                  "add $2, %%bx\n\t"
+                  "loop .go_on_read\n\t"
+                  "ret\n\t"
                  : /* no outputs */
-                 : "c"(sector_no), "a"(start_sector), "b"(mem_to)
-                 : "eax", "ebx", "ecx"
+                 : "c" (sector_no), "a" (start_sector), "b" (mem_to)
+                 // : "eax", "ebx", "ecx"
                  );
 }
