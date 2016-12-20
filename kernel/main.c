a728bdcae2f915e392b673be9a5e610681966f64
neo1218 <neo1218@yeah.net>
2016-12-20 18:22:19 +0800

    update kernel main

--- kernel/main.c
+++ kernel/main.c
@@ -3,48 +3,40 @@
  * ``````
  *   uiharu kernel main
  */
-
 #include "print.h"
 #include "init.h"
-#include "debug.h"
-#include "string.h"
-#include "stdint.h"
+#include "thread.h"
 #include "interrupt.h"
 
 void k_thread_a(void*);
 void k_thread_b(void*);
 
 int main(void) {
-    print_string("This is uiharu kernel\n");
-    init_all();
+   print_string("This is uiharu kernel\n");
+   init_all();
 
-    intr_disable();
-    // create threads
-    thread_start("k_thread_a", 5, k_thread_a, "misaka ");
-    thread_start("k_thread_b", 4, k_thread_b, "kuroko ");
-    // main thread
-    while (1) {
-        print_string("Main ");
-    }
-    intr_enable();
+   thread_start("k_thread_a", 31, k_thread_a, "misaka ");
+   thread_start("k_thread_b", 8, k_thread_b, "kuroko ");
 
-    return 0;        
+   intr_enable();	// 打开中断,使时钟中断起作用
+
+   while(1) {
+      print_string("Main ");
+   };
+
+   return 0;
 }
 
-void k_thread_a(void* arg) {
-    /* thread run in kernel */
-    char* para = arg;  // cast
-    while (1) {
-        // print_string(para);
-    }
-    // print_string(para);
+void k_thread_a(void* arg) {     
+   char* para = arg;
+   while(1) {
+      print_string(para);
+   }
 }
 
-void k_thread_b(void* arg) {
-    /* thread run in kernel */
-    char* para = arg;  // cast
-    while (1) {
-        print_string(para);
-    }
-    // print_string(para);
+void k_thread_b(void* arg) {     
+   char* para = arg;
+   while(1) {
+      print_string(para);
+   }
 }
